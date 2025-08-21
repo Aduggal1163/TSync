d4e73c703096f2a9ce198d7b5a2224e52e761fe3
akshit3456 <akshitkansal0402004@gmail.com>
2025-08-21 18:39:58 +0530

    account, member, project and roles models

---
+++ Backend/model/roles-permission.model.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+import { Permissions, Roles } from "../enums/role.enum.js";
+import { RolePermissions } from "../utils/role-permission.js";
+
+const roleSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        enum: Object.values(Roles),
+        required: true,
+        unique: true,
+    },
+    permissions:{
+        type: [String],
+        enum: Object.values(Permissions),
+        required: true,
+        default: function(){
+            return RolePermissions[this.name];
+        },
+    },
+},
+    {
+        timestamps: true,
+    }
+);
+
+const RoleModel = mongoose.model("Role",roleSchema);
+export default RoleModel;
